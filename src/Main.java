f183caa15169dbc53f476a3d43665ed30459c6ee
123 <argeniraliev27@gmail.com>
2022-08-26 20:01:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Oblast.BATKEN+">>>___aigul gulu");
+        System.out.println(Oblast.OSH+">>>______sulaiman Too");
+        System.out.println(Oblast.CHUY+">>>_____много национальный област");
+        System.out.println(Oblast.JALAL_ABAD+">_arslanbab tokoyu");
+        System.out.println(Oblast.NARYN+">>>__TASH_RABAT");
+        System.out.println(Oblast.TALAS+">>___MANAS baatyr");
+        System.out.println(Oblast.YSSYK_KOL+">_YSSYK_KOL_");
+
+    }
+}
---
+++ src/Oblast.java
@@ -0,0 +1,16 @@
+public enum Oblast {
+    BATKEN,
+    OSH,
+    JALAL_ABAD,
+    YSSYK_KOL,
+    TALAS,
+    CHUY,
+    NARYN;
+
+    public void Oblast() {
+    }
+        public void oblast() {
+            System.out.println("областтын озгочолугу");
+        }
+    }
+
